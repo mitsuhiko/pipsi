7fa8b76bc58e9cbff2ff45c7a332fac4240179f5
Armin Ronacher <armin.ronacher@active-4.com>
2014-08-18 20:50:07 +0200

    This is 0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README', 'rb') as f:
 
 setup(
     name='pipsi',
-    version='0.7',
+    version='0.8',
     description='Wraps pip and virtualenv to install scripts',
     long_description=readme,
     license='BSD',
